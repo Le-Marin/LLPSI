e5f5836d9b7fbad043578ab12ee902f00dbcc2da
Le-Marin <119621011+Le-Marin@users.noreply.github.com>
2023-10-31 03:25:06 +0300

    Update

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 'use strict';
 
+const src = (document.currentScript || [...document.scripts].pop() || {src: ''}).src;
+const ver = (src.match(/\?v=(\d+)/) || '01')[1];
 const basePath = 'https://le-marin.github.io/LLPSI/';
 
-loadScript(basePath + 'js/tip.js?v=' + Date.now());
+loadScript(basePath + 'js/tip.js?v=' + ver);
 
-if (location.protocol === 'file:') {
-  loadScript('js/_edit.js');
-}
+if (location.protocol === 'file:') loadScript('js/_edit.js');
+else loadScript('/footer.js?v=' + ver);
 
 function loadScript(src) {
   const script = document.createElement('script');
   script.src = src;
-  document.head.appendChild(script);
-  script.remove();
+  document.head.appendChild(script).remove();
 }
